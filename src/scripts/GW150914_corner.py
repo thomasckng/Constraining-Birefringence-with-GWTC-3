74bd01ce6b59e91a8599706b2dd25d1b60479752
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2022-08-26 07:08:48 -0400

    Update Content

--- src/scripts/GW150914_corner.py
+++ src/scripts/GW150914_corner.py
@@ -4,8 +4,12 @@ import numpy as np
 import seaborn as sns
 from bilby.gw.result import CBCResult
 import paths
+from matplotlib import rc
 
 sns.set_theme(palette='colorblind', font_scale=1.2)
+
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
 
 nsamples = 5000
 
@@ -14,17 +18,17 @@ with h5py.File(paths.data/"IGWN-GWTC2p1-v2-GW150914_095045_PEDataRelease_mixed_n
     result_ligo = pd.DataFrame.from_records(f["posterior_samples"][()])
 result_ligo = result_ligo.sample(n=nsamples)
 result_ligo['kappa'] = np.zeros(len(result_ligo))
-result_ligo['with'] = np.full(len(result_ligo), "GR (LIGO)")
+result_ligo['with'] = np.full(len(result_ligo), r"GR (LIGO)")
 
 result_bilby = CBCResult.from_json(filename=paths.data/"GW150914_birefringence.json").posterior
 result_bilby = result_bilby.sample(n=nsamples)
-result_bilby['with'] = np.full(len(result_bilby), "Non-GR (frequency dependent)")
+result_bilby['with'] = np.full(len(result_bilby), r"Non-GR (frequency dependent)")
 result_bilby['cos_iota'] = np.cos(result_bilby['iota'])
 
 result_extra = CBCResult.from_json(filename=paths.data/"GW150914_birefringence(frequency_independent).json").posterior
 result_extra = result_extra.sample(n=nsamples)
 result_extra['kappa'] = result_extra['kappa'] * 1000 # different scale was used in the frequency independent data
-result_extra['with'] = np.full(len(result_extra), "Non-GR (frequency independent)")
+result_extra['with'] = np.full(len(result_extra), r"Non-GR (frequency independent)")
 result_extra['cos_iota'] = np.cos(result_extra['iota'])
 
 result = pd.concat([result_bilby,result_extra,result_ligo], ignore_index=True)
@@ -32,13 +36,13 @@ result = pd.concat([result_bilby,result_extra,result_ligo], ignore_index=True)
 g = sns.pairplot(result,
                  vars=['kappa', 'luminosity_distance', 'cos_iota'],
                  corner=True, kind='kde', hue='with',
-                 diag_kws=dict(common_norm=False), plot_kws=dict(common_norm=False, levels=[(1.-0.954499736),(1.-0.682689492)]))
+                 diag_kws=dict(common_norm=False), plot_kws=dict(common_norm=False, levels=[(1.-0.90),(1.-0.3935)]))
 
-g.axes[2,0].set_xlabel("$\kappa$")
-g.axes[1,0].set_ylabel("$d_L$")
-g.axes[2,1].set_xlabel("$d_L$")
-g.axes[2,0].set_ylabel("$\cos\iota$")
-g.axes[2,2].set_xlabel("$\cos\iota$")
+g.axes[2,0].set_xlabel(r"$\kappa$")
+g.axes[1,0].set_ylabel(r"$d_L$")
+g.axes[2,1].set_xlabel(r"$d_L$")
+g.axes[2,0].set_ylabel(r"$\cos\iota$")
+g.axes[2,2].set_xlabel(r"$\cos\iota$")
 g.fig.legends[0].set_bbox_to_anchor((0.65,0.8))
 
-g.savefig(fname=paths.figures/"GW150914_corner.png", bbox_inches="tight", dpi=300)
+g.savefig(fname=paths.figures/"GW150914_corner.pdf", bbox_inches="tight", dpi=300)
--- src/scripts/birefringence.py
+++ src/scripts/birefringence.py
@@ -4,22 +4,26 @@ import lalsimulation as lalsim
 import lal
 import seaborn as sns
 import paths
+from matplotlib import rc
 
 sns.set_theme(palette='colorblind', font_scale=1.2)
 
-approximant = lalsim.SimInspiralGetApproximantFromString("IMRPhenomXPHM")
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+
+approximant = lalsim.SimInspiralGetApproximantFromString("IMRPhenomD")
 
 # frequency array parameters
 df = 0.25
 f_min = 20
-f_max = 3000
+f_max = 2030
 f_ref = 100
 
 # source parameters
 m1_msun = 10
 m2_msun = 10
-chi1 = [0, 0, 0.5]
-chi2 = [0, 0, 0.5]
+chi1 = [0, 0, 0]
+chi2 = [0, 0, 0]
 dist_mpc = 1000
 inclination = np.pi * 0.9
 phi_ref = 0
@@ -36,44 +40,24 @@ hp, hc = lalsim.SimInspiralChooseFDWaveform(m1_kg, m2_kg,
                                             df, f_min, f_max, f_ref,
                                             None, approximant)
 
-hl = (hp.data.data - (hc.data.data * 1j)) /np.sqrt(2)
-hr = (hp.data.data + (hc.data.data * 1j)) /np.sqrt(2)
+hl = (hp.data.data + (hc.data.data * 1j)) /np.sqrt(2)
+hr = (hp.data.data - (hc.data.data * 1j)) /np.sqrt(2)
 
 freq = np.arange(len(hl))*df
 
-kappa = 0.5
+kappa = -0.5
 
 hl_bi_L = hl * np.exp(kappa*(dist_mpc/1000)*(freq/100))
 hr_bi_L = hr * np.exp(-kappa*(dist_mpc/1000)*(freq/100))
 
-hl_bi_R = hl * np.exp(-kappa*(dist_mpc/1000)*(freq/100))
-hr_bi_R = hr * np.exp(kappa*(dist_mpc/1000)*(freq/100))
-
-plt.figure(figsize=(5,11))
-
-plt.subplot(311)
-plt.loglog(freq, abs(hl), label="L")
-plt.loglog(freq, abs(hr), ls='--', label="R")
-plt.xlim(f_min,f_max)
-plt.ylim(1e-40,1e-20)
-plt.title("GR")
+plt.loglog(freq, abs(hl), ls='--', color=sns.color_palette()[0], label=r"L (GR)")
+plt.loglog(freq, abs(hr), ls='--', color=sns.color_palette()[3], label=r"R (GR)")
+plt.loglog(freq, abs(hl_bi_L), color=sns.color_palette()[0], label=r"L (br)")
+plt.loglog(freq, abs(hr_bi_L), color=sns.color_palette()[3], label=r"R (br)")
+plt.xlim(f_min+1,f_max-10)
+plt.ylim(1e-38,1e-20)
 plt.legend()
-
-plt.subplot(312)
-plt.loglog(freq, abs(hl_bi_L), label="L")
-plt.loglog(freq, abs(hr_bi_L), ls='--', label="R")
-plt.xlim(f_min,f_max)
-plt.ylim(1e-40,1e-20)
 plt.ylabel(r"$|\tilde{h}|$")
-plt.title("Birefringence (left-handed)")
+plt.xlabel(r"$f\mathrm{(Hz)}$")
 
-plt.subplot(313)
-plt.loglog(freq, abs(hl_bi_R), label="L")
-plt.loglog(freq, abs(hr_bi_R), ls='--', label="R")
-plt.xlim(f_min,f_max)
-plt.ylim(1e-40,1e-20)
-plt.xlabel("frequency (Hz)")
-plt.title("Birefringence (right-handed)")
-
-plt.subplots_adjust(hspace=0.3)
-plt.savefig(fname=paths.figures/"birefringence.png", bbox_inches="tight", dpi=300)
+plt.savefig(fname=paths.figures/"birefringence.pdf", bbox_inches="tight", dpi=300)
