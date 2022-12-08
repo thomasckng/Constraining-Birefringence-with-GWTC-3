c9a127635d53dbb51207fe66cfdd8d246cdd2364
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2022-12-08 20:03:48 +0800

    Fix latex text

--- src/scripts/Gaussian_result.py
+++ src/scripts/Gaussian_result.py
@@ -10,7 +10,7 @@ mu_median = np.median(df['mu'])
 sigma_median = np.median(df['sigma'])
 
 with open(paths.output/"mu_median.txt", "w") as f:
-    f.write(f"${mu_median:.3f}^{(np.percentile(a=df['mu'],q=95)-mu_median):.3f}_{(np.percentile(a=df['mu'],q=5)-mu_median):.3f}$")
+    f.write(rf"${mu_median:.3f}^{{{(np.percentile(a=df['mu'],q=95)-mu_median):.3f}}}_{{{(np.percentile(a=df['mu'],q=5)-mu_median):.3f}}}$")
 
 with open(paths.output/"sigma_median.txt", "w") as f:
-    f.write(f"${sigma_median:.3f}^{(np.percentile(a=df['sigma'],q=95)-sigma_median):.3f}_{(np.percentile(a=df['sigma'],q=5)-sigma_median):.3f}$")
+    f.write(rf"${sigma_median:.3f}^{{{(np.percentile(a=df['sigma'],q=95)-sigma_median):.3f}}}_{{{(np.percentile(a=df['sigma'],q=5)-sigma_median):.3f}}}$")
