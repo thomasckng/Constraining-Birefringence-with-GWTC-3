d61e2ce6c41345528fc27fbea4faa90c745e3634
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-02-17 07:19:05 +0800

    Add reweighed kappa distributions

---
+++ src/scripts/reweighed_kappa.py
@@ -0,0 +1,22 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import paths
+
+sns.set_theme(palette='colorblind', font_scale=1.2)
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"],
+})
+
+df_kappa = pd.read_feather(paths.data/'reweighted_kappa_samples_without_GW200129.feather')
+
+g = sns.histplot(df_kappa.drop(axis=1, labels="GW170818"), element='step', common_norm=False, stat='density', fill=False, binwidth=0.01)
+g.get_legend().remove()
+g.set_xlabel('$\kappa$')
+g.set_ylabel('$p(\kappa|\mu,\sigma)$')
+g.set_xlim(-.2,.2)
+
+plt.savefig(fname=paths.figures/"reweighed_kappa.pdf", bbox_inches="tight", dpi=300)
