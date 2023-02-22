bc0074be4c28ddb717b1c566db6fd7b7e86442f4
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-02-23 02:30:16 +0800

    Change to one-sided limit for Gaussian sigma

--- src/scripts/Gaussian_result.py
+++ src/scripts/Gaussian_result.py
@@ -13,4 +13,4 @@ with open(paths.output/"mu_median.txt", "w") as f:
     f.write(rf"${mu_median:.3f}^{{+{(np.percentile(a=df['mu'],q=95)-mu_median):.3f}}}_{{{(np.percentile(a=df['mu'],q=5)-mu_median):.3f}}}$")
 
 with open(paths.output/"sigma_median.txt", "w") as f:
-    f.write(rf"${sigma_median:.3f}^{{+{(np.percentile(a=df['sigma'],q=95)-sigma_median):.3f}}}_{{{(np.percentile(a=df['sigma'],q=5)-sigma_median):.3f}}}$")
+    f.write(rf"${np.percentile(a=df['sigma'],q=90):.3f}$")
