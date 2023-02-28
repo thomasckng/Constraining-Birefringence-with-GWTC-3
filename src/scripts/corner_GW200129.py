bb5a0881a153c11abfe2997fb4adebb3906de31f
max.isi <max.isi@ligo.org>
2023-02-27 21:25:02 -0500

    GW200129 plot

--- src/scripts/corner_GW200129.py
+++ src/scripts/corner_GW200129.py
@@ -18,36 +18,60 @@ nsamples = 5000
 result = pd.read_feather(paths.data/"samples_posterior_birefringence.feather")
 result = result[result.event == "GW200129_065458"]
 result = result.sample(n=nsamples)
-result['with'] = np.full(len(result), r"birefringence (H1, L1, V1)")
+result['with'] = np.full(len(result), "HLV")
 result['cos_iota'] = np.cos(result['iota'])
 
 result_without_H1 = CBCResult.from_json(filename=paths.data/"GW200129_065458_birefringence_without_H1.json.gz").posterior
 result_without_H1 = result_without_H1.sample(n=nsamples)
-result_without_H1['with'] = np.full(len(result_without_H1), r"birefringence (L1, V1)")
+result_without_H1['with'] = np.full(len(result_without_H1), "LV")
 result_without_H1['cos_iota'] = np.cos([float(result_without_H1['iota'][i]) for i in result_without_H1.index])
 
 result_without_L1 = CBCResult.from_json(filename=paths.data/"GW200129_065458_birefringence_without_L1.json.gz").posterior
 result_without_L1 = result_without_L1.sample(n=nsamples)
-result_without_L1['with'] = np.full(len(result_without_L1), r"birefringence (H1, V1)")
+result_without_L1['with'] = np.full(len(result_without_L1), "HV")
 result_without_L1['cos_iota'] = np.cos([float(result_without_L1['iota'][i]) for i in result_without_L1.index])
 
 result_without_V1 = CBCResult.from_json(filename=paths.data/"GW200129_065458_birefringence_without_V1.json.gz").posterior
 result_without_V1 = result_without_V1.sample(n=nsamples)
-result_without_V1['with'] = np.full(len(result_without_V1), r"birefringence (H1, L1)")
+result_without_V1['with'] = np.full(len(result_without_V1), "HL")
 result_without_V1['cos_iota'] = np.cos([float(result_without_V1['iota'][i]) for i in result_without_V1.index])
 
 result_all = pd.concat([result,result_without_H1,result_without_L1,result_without_V1], ignore_index=True)
+#result_all = pd.concat([result,result_without_H1,result_without_L1], ignore_index=True)
 
-g = sns.pairplot(result_all,
+def kdeplot(x, y=None, hue=None, mask=None, **kws):
+    if y is not None:
+        y = y[mask]
+    sns.kdeplot(x=x[mask], y=y, hue=hue[mask], **kws)
+
+p = list(sns.color_palette())[:3] + ['0.35'] # [sns.color_palette()[4]]
+g = sns.PairGrid(data=result_all,
                  vars=['kappa', 'luminosity_distance', 'cos_iota'],
-                 corner=True, kind='kde', hue='with',
-                 diag_kws=dict(common_norm=False, fill=False), plot_kws=dict(common_norm=False, levels=[(1.-0.90)]))
+                 corner=True, hue='with', diag_sharey=False,
+                 layout_pad=0., palette=p,
+                )
+m = result_all['with'] == "HLV"
+g.map_lower(kdeplot, mask=m, common_norm=False, levels=[1.-0.90, 0.9999], fill=True, alpha=0.5)
+g.map_lower(kdeplot, mask=m, common_norm=False, levels=[1.-0.90, 0.9999])
+g.map_diag(kdeplot, mask=m, common_norm=False, fill=True)
+
+m = (result_all['with'] == "HV") | (result_all['with'] == "LV")
+g.map_lower(kdeplot, mask=m, linewidths=2, common_norm=False, levels=[(1.-0.90)])
+g.map_diag(kdeplot, mask=m, linewidth=2, common_norm=False, fill=False)
+
+m = result_all['with']=="HL"
+g.map_lower(kdeplot, mask=m, linewidths=2, linestyles='--', common_norm=False, levels=[(1.-0.90)], zorder=100)
+g.map_diag(kdeplot, mask=m, linewidth=2, linestyle='--', common_norm=False, fill=False, zorder=100)
 
 g.axes[2,0].set_xlabel(r"$\kappa$")
 g.axes[1,0].set_ylabel(r"$d_L$")
 g.axes[2,1].set_xlabel(r"$d_L$")
 g.axes[2,0].set_ylabel(r"$\cos\iota$")
 g.axes[2,2].set_xlabel(r"$\cos\iota$")
-g.fig.legends[0].set_bbox_to_anchor((0.65,0.8))
+#g.fig.legends[0].set_bbox_to_anchor((0.65,0.8))
+
+for k, c in zip(["HLV", "HV", "LV", "HL"], p):
+    g.axes[0,0].plot([], c=c, lw=2, ls='--' if k=="HL" else '-', label=k)
+g.axes[0,0].legend(loc='center left', bbox_to_anchor=(1.1, 0.5))
 
 g.savefig(fname=paths.figures/"corner_GW200129.pdf", bbox_inches="tight", dpi=300)
--- src/scripts/corner_Gaussian.py
+++ src/scripts/corner_Gaussian.py
@@ -7,6 +7,8 @@ import paths
 
 sns.set_theme(palette='colorblind', font_scale=1.2)
 
+nsamp = 5000
+
 plt.rcParams.update({
     "text.usetex": True,
     "font.family": "serif",
@@ -21,7 +23,7 @@ sigma_median = np.median(samples_Gaussian[:,1])
 df = pd.DataFrame()
 df['mu'] = samples_Gaussian.reshape(-1,2)[:,0]
 df['sigma'] = samples_Gaussian.reshape(-1,2)[:,1]
-df = df.sample(10000)
+df = df.sample(nsamp)
 
 kde_1d_sigma = kde_contour.Bounded_1d_kde(df['sigma'], xlow=0)
 df['sigma_kde'] = kde_1d_sigma(df['sigma'])
@@ -35,7 +37,7 @@ sigma_median_without_GW200129 = np.median(samples_Gaussian_without_GW200129[:,1]
 df_without_GW200129 = pd.DataFrame()
 df_without_GW200129['mu'] = samples_Gaussian_without_GW200129.reshape(-1,2)[:,0]
 df_without_GW200129['sigma'] = samples_Gaussian_without_GW200129.reshape(-1,2)[:,1]
-df_without_GW200129 = df_without_GW200129.sample(10000)
+df_without_GW200129 = df_without_GW200129.sample(nsamp)
 
 kde_1d_sigma_without_GW200129 = kde_contour.Bounded_1d_kde(df_without_GW200129['sigma'], xlow=0)
 df_without_GW200129['sigma_kde'] = kde_1d_sigma_without_GW200129(df_without_GW200129['sigma'])
@@ -44,22 +46,31 @@ df_without_GW200129 = df_without_GW200129.sort_values(['sigma'])
 fig, axs = plt.subplots(2,2,sharex='col')
 fig.set_size_inches(5,5)
 
-sns.kdeplot(df['mu'], fill=True, ax=axs[0,0])
-sns.kdeplot(df_without_GW200129['mu'], fill=True, ax=axs[0,0])
-axs[0,0].axvline(mu_median, color=sns.color_palette()[0], label='Include')
-axs[0,0].axvline(mu_median_without_GW200129, color=sns.color_palette()[3], label='Exclude')
+lkws = dict(ls='--', c='k')
+axs[0,0].axvline(0, **lkws)
+axs[1,0].axvline(0, **lkws)
+axs[1,0].axhline(0, **lkws)
+axs[1,1].axvline(0, **lkws)
+
+c0 = sns.color_palette()[0]
+c1 = sns.color_palette()[3]
+
+#axs[0,0].axvline(mu_median, color=c0)
+sns.kdeplot(df['mu'], fill=True, ax=axs[0,0], color=c0)
+#axs[0,0].axvline(mu_median_without_GW200129, color=c1)
+sns.kdeplot(df_without_GW200129['mu'], fill=True, ax=axs[0,0], color=c1)
 
 axs[0,0].set_ylabel("")
 axs[0,0].set_xlim(-.07,.07)
 axs[0,0].set_ylim(0)
 axs[0,0].set_yticks([])
 
-axs[1,1].fill_between(df['sigma'], df['sigma_kde'], np.zeros(len(df['sigma'])), alpha=0.2)
-axs[1,1].plot(df['sigma'], df['sigma_kde'])
-axs[1,1].fill_between(df_without_GW200129['sigma'], df_without_GW200129['sigma_kde'], np.zeros(len(df_without_GW200129['sigma'])), alpha=0.2)
-axs[1,1].plot(df_without_GW200129['sigma'], df_without_GW200129['sigma_kde'])
-axs[1,1].axvline(sigma_median, color=sns.color_palette()[0])
-axs[1,1].axvline(sigma_median_without_GW200129, color=sns.color_palette()[3])
+#axs[1,1].axvline(sigma_median, color=c0)
+axs[1,1].fill_between(df['sigma'], df['sigma_kde'], np.zeros(len(df['sigma'])), alpha=0.2, color=c0)
+axs[1,1].plot(df['sigma'], df['sigma_kde'], c=c0)
+#axs[1,1].axvline(sigma_median_without_GW200129, color=sns.color_palette()[3])
+axs[1,1].fill_between(df_without_GW200129['sigma'], df_without_GW200129['sigma_kde'], np.zeros(len(df_without_GW200129['sigma'])), alpha=0.2, color=c1)
+axs[1,1].plot(df_without_GW200129['sigma'], df_without_GW200129['sigma_kde'], color=c1)
 
 axs[1,1].set_xlabel("$\\sigma$")
 axs[1,1].set_xlim(0)
@@ -67,12 +78,12 @@ axs[1,1].set_ylim(0)
 axs[1,1].set_yticks([])
 axs[1,1].get_lines()[1].set_linewidth(1)
 
-kde_contour.kdeplot_2d_clevels(xs=df['mu'], ys=df['sigma'], ylow=0, ax=axs[1,0], color=sns.color_palette()[0], levels=[0.90,0.3935])
-kde_contour.kdeplot_2d_clevels(xs=df_without_GW200129['mu'], ys=df_without_GW200129['sigma'], ylow=0, ax=axs[1,0], color=sns.color_palette()[3], levels=[0.90,0.3935])
-axs[1,0].axvline(mu_median, color=sns.color_palette()[0])
-axs[1,0].axhline(sigma_median, color=sns.color_palette()[0])
-axs[1,0].axvline(mu_median_without_GW200129, color=sns.color_palette()[3])
-axs[1,0].axhline(sigma_median_without_GW200129, color=sns.color_palette()[3])
+# axs[1,0].axvline(mu_median, color=sns.color_palette()[0])
+# axs[1,0].axhline(sigma_median, color=sns.color_palette()[0])
+kde_contour.kdeplot_2d_clevels(xs=df['mu'], ys=df['sigma'], ylow=0, ax=axs[1,0], color=c0, levels=[0.90,0.3935])
+#axs[1,0].axvline(mu_median_without_GW200129, color=sns.color_palette()[3])
+#axs[1,0].axhline(sigma_median_without_GW200129, color=sns.color_palette()[3])
+kde_contour.kdeplot_2d_clevels(xs=df_without_GW200129['mu'], ys=df_without_GW200129['sigma'], ylow=0, ax=axs[1,0], color=c1, levels=[0.90,0.3935])
 
 axs[1,0].set_xlabel("$\\mu$")
 axs[1,0].set_ylabel("$\\sigma$")
