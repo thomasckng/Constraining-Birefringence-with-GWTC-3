972446f58ac0d6eb0efaefc020b8b19beacd4649
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-04-05 23:51:22 +0800

    Edit table 2 columns

--- src/scripts/bimodal_events_mass.py
+++ src/scripts/bimodal_events_mass.py
@@ -4,10 +4,16 @@ import scipy
 from bilby.gw.result import CBCResult
 import paths
 
+def sign(x):
+    if x > 0:
+        return "+"
+    elif x <= 0:
+        return ""
+
 result_DataFrame = pd.read_feather(paths.data/"samples_posterior_birefringence.feather")
 
 with open(paths.output/"bimodal_events_mass.txt", "w") as f:
-    f.write(r"\begin{tabular}{lcccc}Event & $M$ ($M_{\odot}$) & $\chi_p$ (GR) & $\chi_p$ (BR) & CL\\ \hline ")
+    f.write(r"\begin{tabular}{lcccc}Event & $M$ ($M_{\odot}$) & $\chi_p$ & $\chi_{\rm eff}$ & CL\\ \hline ")
     for event in ["GW170104", "GW190413_134308", "GW190521", "GW190805_211137", "GW191105_143521"]:
         name = event.replace("_", r"\_")
 
@@ -23,19 +29,18 @@ with open(paths.output/"bimodal_events_mass.txt", "w") as f:
         chi_p_GR_95 = chi_p_GR.quantile(0.95) - chi_p_GR_median
         chi_p_GR_5 = chi_p_GR_median - chi_p_GR.quantile(0.05)
 
-        chi_p_BR = result_DataFrame[result_DataFrame.event == event]['chi_p']
-        chi_p_BR_median  = chi_p_BR.median()
-        chi_p_BR_95 = chi_p_BR.quantile(0.95) - chi_p_BR_median
-        chi_p_BR_5 = chi_p_BR_median - chi_p_BR.quantile(0.05)
+        chi_eff_GR = result_DataFrame[result_DataFrame.event == event]['chi_eff']
+        chi_eff_GR_median  = chi_eff_GR.median()
+        chi_eff_GR_95 = chi_eff_GR.quantile(0.95) - chi_eff_GR_median
+        chi_eff_GR_5 = chi_eff_GR_median - chi_eff_GR.quantile(0.05)
 
         kappa = result_DataFrame[result_DataFrame.event == event]['kappa'].values
 
         kernel = scipy.stats.gaussian_kde(kappa)
-        kde = [kernel(kappa) for kappa in kappa]
-        kde = np.array(kde)
+        kde = np.array([kernel(kappa) for kappa in kappa])
         credible_level = len(kde[kde > kernel(0)[0]])/len(kde)
 
-        f.write(rf"{name} & ${mass_median:.1f}^{{+{mass_95:.1f}}}_{{-{mass_5:.1f}}}$ & ${chi_p_GR_median:.2f}^{{+{chi_p_GR_95:.2f}}}_{{-{chi_p_GR_5:.2f}}}$ & ${chi_p_BR_median:.2f}^{{+{chi_p_BR_95:.2f}}}_{{-{chi_p_BR_5:.2f}}}$ & ${credible_level:.3f}$")
+        f.write(rf"{name} & ${mass_median:.1f}^{{+{mass_95:.1f}}}_{{-{mass_5:.1f}}}$ & ${chi_p_GR_median:.2f}^{{+{chi_p_GR_95:.2f}}}_{{-{chi_p_GR_5:.2f}}}$ & ${sign(chi_eff_GR_median)}{chi_eff_GR_median:.2f}^{{+{chi_eff_GR_95:.2f}}}_{{-{chi_eff_GR_5:.2f}}}$ & ${credible_level:.3f}$")
         if event != "GW191105_143521":
             f.write(r"\\")
     f.write(r" \end{tabular}")
